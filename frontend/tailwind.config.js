f212cce4c92212b482f8d4adeac246091bcec1d1
andra-sun <camileguimaraes463@gmail.com>
2025-11-19 14:42:37 -0300

    äjusta tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-      content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+module.exports = {
+   content: ["./app/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             colors: {
