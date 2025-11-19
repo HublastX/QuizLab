e7ad2d8505ab8115720dc2c06773348932cb5218
andra-sun <camileguimaraes463@gmail.com>
2025-11-19 12:11:09 -0300

    volta o next config antigo

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/quiz-lab",
-    assetPrefix: "/quiz-lab",
-    trailingSlash: true,
+import type { NextConfig } from "next";
 
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
