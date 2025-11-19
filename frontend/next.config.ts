f05587d8d236c2a0c607a8d4007470ba1b93afa2
andra-sun <camileguimaraes463@gmail.com>
2025-11-19 17:04:11 -0300

    coloca rota no next config e redirect tambem

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/quiz-lab',
   assetPrefix: '/quiz-lab',
+  
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',  // automaticamente vira /quiz-lab/home
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
