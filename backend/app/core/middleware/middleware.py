6a636c73d0b4721141e99bc7a65bd43d52566f42
wendellast <wendelalves01a@hotmail.com>
2025-11-19 09:42:34 -0300

    refactor: substitui middleware cors manual por corsmiddleware do fastapi

--- backend/app/core/middleware/middleware.py
+++ backend/app/core/middleware/middleware.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 def cors_middleware(app: FastAPI):
-    @app.middleware("http")
-    async def cors_middleware(request: Request, call_next):
-        response = await call_next(request)
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Methods"] = "*"
-        response.headers["Access-Control-Allow-Headers"] = "*"
-        return response
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
