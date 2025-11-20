4edbb5577a2d73a4f6bfe1b87a56f8f439823d24
wendellast <wendelalves01a@hotmail.com>
2025-11-20 16:58:37 -0300

    feat: adiciona componente themepage com marcação inicial

--- frontend/src/app/(home)/theme/page.tsx
+++ frontend/src/app/(home)/theme/page.tsx
@@ -1,1 +1,9 @@
-"tirando pelo meu mondei dps pegua o codigo no git e poe aqui"
+"use client";
+
+export default function ThemePage() {
+    return (
+        <div>
+            <h1>REMOVER pelo meu dinheiro dps tu poe</h1>
+        </div>
+    );
+}
