f757569598046ef37004257393154be54fe4a3eb
veiRL <symain445@gmail.com>
2025-11-20 15:54:56 -0300

    adicionado cards.json

--- frontend/src/app/(home)/home/page.tsx
+++ frontend/src/app/(home)/home/page.tsx
@@ -35,31 +35,6 @@ export default function Home() {
                             color={card.color}
                         />
                     ))} */}
-
-                    <HomeCard
-                        title="Título 1"
-                        icon={<CiAlarmOn />}
-                        description="Descrição 1"
-                        href="#"
-                        ariaLabel="Título 1 - Descrição 1"
-                        color="create"
-                    />
-                    <HomeCard
-                        title="Título 2"
-                        icon={<CiAlarmOn />}
-                        description="Descrição 2"
-                        href="#"
-                        ariaLabel="Título 2 - Descrição 2"
-                        color="ranking"
-                    />
-                    <HomeCard
-                        title="Título 3"
-                        icon={<CiAlarmOn />}
-                        description="Descrição 3"
-                        href="#"
-                        ariaLabel="Título 3 - Descrição 3"
-                        color="play"
-                    />
                 </div>
                 <Acessibilidade />
             </div>
@@ -67,69 +42,7 @@ export default function Home() {
                 <div className="grid grid-cols-1 gap-6">
                     <h2 className="text-2xl font-black">Temas</h2>
                     <div className="grid grid-cols-3 gap-6 overflow-auto">
-                        <HomeCard
-                            title="pitagoras"
-                            icon={<CiBasketball />}
-                            description="Descrição 3"
-                            href="#"
-                            ariaLabel="Título 3 - Descrição 3"
-                            color="play"
-                            variant="theme"
-                        />
-                        <HomeCard
-                            title="playstation"
-                            icon={<CiBasketball />}
-                            description="Descrição 3"
-                            href="#"
-                            ariaLabel="Título 3 - Descrição 3"
-                            color="play"
-                            variant="theme"
-                        />
-                        <HomeCard
-                            title="Arvore"
-                            icon={<CiBasketball />}
-                            description="Descrição 3"
-                            href="#"
-                            ariaLabel="Título 3 - Descrição 3"
-                            color="play"
-                            variant="theme"
-                        />
-                        <HomeCard
-                            title="luva de pedreiro"
-                            icon={<CiBasketball />}
-                            description="Descrição 3"
-                            href="#"
-                            ariaLabel="Título 3 - Descrição 3"
-                            color="play"
-                            variant="theme"
-                        />
-                        <HomeCard
-                            title="luva de pedreiro"
-                            icon={<CiBasketball />}
-                            description="Descrição 3"
-                            href="#"
-                            ariaLabel="Título 3 - Descrição 3"
-                            color="play"
-                            variant="theme"
-                        />
-                        <HomeCard
-                            title="luva de pedreiro"
-                            icon={<CiBasketball />}
-                            description="Descrição 3"
-                            href="#"
-                            ariaLabel="Título 3 - Descrição 3"
-                            color="play"
-                            variant="theme"
-                        />
-                        <HomeCard
-                            title="luva de pedreiro"
-                            icon={<CiBasketball />}
-                            description="Descrição 3"
-                            href="#"
-                            ariaLabel="Título 3 - Descrição 3"
-                            color="play"
-                            variant="theme"
-                        />
+
                     </div>
                 </div>
             </div>
