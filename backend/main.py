9dd64b6ee30f4a093ad6fd4e677d476d753ce455
wendellast <wendelalves01a@hotmail.com>
2025-11-19 23:58:59 -0300

    build: substitui criacao de tabelas por migracoes e adiciona dependencias

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
 from app.api.routes.router import register_routes
-from app.core.database.database import create_all_tables
 from app.core.middleware.middleware import cors_middleware  
 from app.core.settings.settings import settings
 
+from app.core.database.database import run_migrations
 app = FastAPI(root_path="/quiz-lab-backend")
 
 cors_middleware(app)
 register_routes(app)
-create_all_tables()
+#create_all_tables()
+run_migrations()
 
 
 if __name__ == "__main__":
